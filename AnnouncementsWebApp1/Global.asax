20f0a95cb7036ad54c01caccff09563d4c8f76ed
eghar <eghar@DESKTOP-2L3J2P8>
2016-12-15 16:34:28 +0000

    Add project files.

---
+++ AnnouncementsWebApp1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AnnouncementsWebApp1.MvcApplication" Language="C#" %>
---
+++ AnnouncementsWebApp1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AnnouncementsWebApp1.Startup))]
+namespace AnnouncementsWebApp1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
